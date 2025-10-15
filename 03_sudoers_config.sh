f9147e3ca8eae5b03ff9a510a2ad52597537fd00
TheyCallMeTemik <121228090+TheyCallMeTemik@users.noreply.github.com>
2025-10-15 22:24:49 +0500

    Delete 03_sudoers_config.sh

--- 03_sudoers_config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "admin1 ALL=(ALL) ALL" | sudo tee /etc/sudoers.d/admins
-echo "autobot ALL=(ALL) NOPASSWD: /bin/systemctl restart taskboard.service" | sudo tee /etc/sudoers.d/automation
-sudo chmod 440 /etc/sudoers.d/*
